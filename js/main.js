457cc5a0ff237dc2cfd7a2f9ab05fe65124a807b
lukmak0394 <lukmak0394@wp.pl>
2022-06-15 11:55:13 +0200

    finished navbar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
+
+// Mobile navigation show and hide mechanism
+const navBar = document.querySelector('.navbar');
 const menuIcon = document.querySelector('.menu-icon');
 
-const navBar = document.querySelector('.navbar');
+const navOpen = () => {
+    navBar.style.right = '0';
+}
 
-menuIcon.addEventListener('click',function() {
+const navClose = () => {
+    navBar.style.right = '-100%';
+}
+
+menuIcon.addEventListener('click', (e) => {
+
+    e.target.classList.toggle('fa-times');
     
+    e.target.classList.contains('fa-times') ? navOpen() : navClose();
 
-    const menuIconInner = menuIcon.children[0]
-    // Check if navbar by default is opened. 
-    // If yes  - remove class open, add class close (show and hide animation), change icon from X to hamburger.
-    // If not (else) - remove class close, add class open (show and hide animation), change icon from burger to X
-    if (navBar.classList.contains('navbar-opened')) {
-        navBar.classList.remove('navbar-opened');
-        navBar.classList.add('navbar-closed');
-        menuIconInner.classList.remove('fa-times');
-        menuIconInner.classList.add('fa-bars');
-    } else { 
-        navBar.classList.remove('navbar-closed');
-        navBar.classList.add('navbar-opened');
-        menuIconInner.classList.remove('fa-bars');
-        menuIconInner.classList.add('fa-times');
-    }
 });
+
 
